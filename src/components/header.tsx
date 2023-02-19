ba4840c8e468dba900e92e8accfd0a38f6488cb1
Angelnixs <juangon@hotmail.com>
2023-02-19 05:20:47 -0600

    .

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,25 +8,21 @@ import "./styles/header.css"
 export default function Header() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" sticky="top" className="nav-min-height">
-      <Navbar.Brand href="/" className="ms-4"><img
-              alt=""
-              src="/pylc.png"
-              width="40"
-              height="40"
-              className="d-inline-block align-top"
-            />{' '}</Navbar.Brand>
+      <Navbar.Brand href="/" className="ms-4">
+        <img alt="" src="/pylc.png" width="40" height="40" className="d-inline-block align-top"/>
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" className="mx-3" />        
       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-between mx-3">
         <Nav>
           <Nav.Link href="/">Inicio</Nav.Link>
           <Nav.Link href="/listaPaquetes">Lista de Paquetes</Nav.Link>
+          <Nav.Link href="/login" className='d-menu-show-mobile'>Iniciar Sesión</Nav.Link>
         </Nav>
       </Navbar.Collapse>
-      <Dropdown className="mx-2 d-menu-hide me-3 pointer" autoClose="outside" as="a">
-        <Dropdown.Toggle id="cart-menu" className="user" as="a">
+      <Dropdown className="mx-2 d-menu-hide-mobile me-3 pointer" autoClose="outside" as="a">
+        <Dropdown.Toggle id="cart-menu" className="icon" as="a">
           <i className="bi bi-cart4"></i>
         </Dropdown.Toggle>
-
         <Dropdown.Menu align="end">
           <Dropdown.Item href="/paquete">Item 1</Dropdown.Item>
           <Dropdown.Divider />
@@ -35,13 +31,12 @@ export default function Header() {
           <Dropdown.ItemText><Button variant="dark" as="a" href="/carrito">Ir A Carrito</Button></Dropdown.ItemText>
         </Dropdown.Menu>
       </Dropdown>
-      <Dropdown className="mx-2 d-menu-hide me-5 pointer" autoClose="outside" as="a">
-        <Dropdown.Toggle id="user-menu" className="user" as="a">
+      <Dropdown className="mx-2 d-menu-hide-mobile me-5 pointer" autoClose="outside" as="a">
+        <Dropdown.Toggle id="user-menu" className="icon" as="a">
           <i className="bi bi-person-fill"></i>
         </Dropdown.Toggle>
-
         <Dropdown.Menu align="end">
-          <Dropdown.Item href="/login">Login</Dropdown.Item>
+          <Dropdown.Item href="/login">Iniciar Sesión</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
     </Navbar>

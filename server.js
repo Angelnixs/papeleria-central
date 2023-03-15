c39bc6f76de76c28e55efc8c256638c8c74585b2
Angelnixs <juangon@hotmail.com>
2023-03-15 00:02:49 -0600

    add-server

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.static('dist', options))
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 const port = process.env.PORT || 3000
